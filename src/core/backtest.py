7e455368b8e0a55ffaaa35f3047d808407e27c6b
ordian <noreply@reusable.software>
2025-05-18 03:19:19 +0200

    try to fix the bug in equity curve

--- src/core/backtest.py
+++ src/core/backtest.py
@@ -65,6 +65,7 @@ def run_backtest(strategy_class, df: pd.DataFrame, initial_capital: float = 1000
     # Track current position and coins
     current_position = 0
     current_coins = 0
+    equity_curve.iloc[0] = initial_capital
     
     # Process each bar
     for i in range(len(df)):
@@ -78,12 +79,12 @@ def run_backtest(strategy_class, df: pd.DataFrame, initial_capital: float = 1000
             current_position = 0
             current_coins = 0
             coins.iloc[i:] = 0
+        else:
+            equity_curve.iloc[i] = equity_curve.iloc[i-1]
         
         # Update equity curve
         if current_position == 1:
             equity_curve.iloc[i] = current_coins * df['close'].iloc[i]
-        else:
-            equity_curve.iloc[i] = equity_curve.iloc[i-1] if i > 0 else initial_capital
     
     # Generate trade records
     trades = []
